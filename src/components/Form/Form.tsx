cde71df329b6e035d4c1b2736f07c27758eaef7e
Vinicius Martins <vinibitmartins@gmail.com>
2025-01-13 21:51:28 -0300

    Refatora componente Form para utilizar função funcional e gerenciar estado com hooks

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 import style from './Form.module.scss'
@@ -14,60 +14,61 @@ const initialState: ITask = {
     completed: false
 }
 
-class Form extends Component<{ 
-    onSubmit: React.Dispatch<React.SetStateAction<ITask[]>> }, 
-    ITask> {
+interface FormProps {
+    onSubmit: React.Dispatch<React.SetStateAction<ITask[]>>
+}
+
+const Form: React.FC<FormProps> = ({ onSubmit }) => {
     
-    state: Readonly<ITask> = initialState    
-    
-    addTask(event: React.FormEvent<HTMLFormElement>) {
-        event.preventDefault()
-        this.props.onSubmit(actualState => [
-            ...actualState, { 
-                ...this.state,
+    const [task, setTask] = useState<ITask>(initialState);
+
+    const addTask = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        onSubmit(actualState => [
+            ...actualState,
+            {
+                ...task,
                 id: uuidv4(),
                 selected: false,
                 completed: false
-            } 
-        ])
-        this.setState(initialState)
+            }
+        ]);
+        setTask(initialState);
     }
- 
-    render() {
-        return (
-            <form className={style.novaTarefa} onSubmit={(event) => this.addTask(event)}>
-                <div className={style.inputContainer}>
-                    <label htmlFor="task">Adicione um novo estudo</label>
-                    <input
-                        type="text"
-                        name="task"
-                        id="task"
-                        placeholder="O que você quer estudar?"
-                        required
-                        value={this.state.name}
-                        onChange={event => this.setState({ ...this.state, name: event.target.value })}                        
-                    />
-                </div>
-                <div className={style.inputContainer}>
-                    <label htmlFor="time">Tempo</label>
-                    <input
-                        type="time"
-                        step={1}
-                        name="time"
-                        id="time"
-                        min={"00:00:00"}
-                        max={"01:30:00"}
-                        required
-                        value={this.state.time}
-                        onChange={event => this.setState({ ...this.state, time: event.target.value })}                        
-                    />
-                </div>
-                <Button type="submit">
-                    Adicionar
-                </Button>                 
-            </form>
-        );
-    }
+
+    return (
+        <form className={style.novaTarefa} onSubmit={addTask}>
+            <div className={style.inputContainer}>
+                <label htmlFor="task">Adicione um novo estudo</label>
+                <input
+                    type="text"
+                    name="task"
+                    id="task"
+                    placeholder="O que você quer estudar?"
+                    required
+                    value={task.name}
+                    onChange={event => setTask({ ...task, name: event.target.value })}
+                />
+            </div>
+            <div className={style.inputContainer}>
+                <label htmlFor="time">Tempo</label>
+                <input
+                    type="time"
+                    step={1}
+                    name="time"
+                    id="time"
+                    min={"00:00:00"}
+                    max={"01:30:00"}
+                    required
+                    value={task.time}
+                    onChange={event => setTask({ ...task, time: event.target.value })}
+                />
+            </div>
+            <Button type="submit">
+                Adicionar
+            </Button>
+        </form>
+    )
 }
 
 export default Form
